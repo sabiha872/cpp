336e6723795a6040e77f18047a626c0d9020a5b0
Sabiha <sabiha.23bce11638@vitbhopal.ac.in>
2026-05-20 15:14:00 +0530

    Add recursion examples

---
+++ directrecursion.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// using namespace std;
+// void fun(int n) {
+//   if(n>0) {
+//     cout << n << " ";
+//     fun(n-1); 
+//   }
+// }
+// int main() {
+//   fun(5);
+  
+// }
+#include <iostream>
+using namespace std;
+
+void funB(int n) ;
+
+void funA(int n) {
+  if(n>0) {
+    cout << n << " ";
+    funB(n-1); 
+  }
+}
+void funB(int n) {
+  if (n>0) {
+    funA(n-1); 
+    cout << n << " ";
+  }
+}
+int main() {
+  funA(7);
+  
+}
