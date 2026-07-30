9b2abbf13bd37f91481440f04c01d1605c9a9a3e
Sabiha <sabiha.23bce11638@vitbhopal.ac.in>
2026-07-30 20:07:04 +0530

    Update Binary tree code

--- Binary tree/code.cpp
+++ Binary tree/code.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Node {
@@ -16,24 +17,56 @@ class Node {
 static int idx = -1;
 Node* buildTree(vector<int> preorder) {
   idx++;
+
 if(preorder[idx] == -1) {
     return NULL;
   }
   Node* root = new Node(preorder[idx]);
   root->left = buildTree(preorder);
   root->right = buildTree(preorder);
-
   return root;
+}
+ // inorder
+ void inorder(Node* root) {
+  if(root == NULL) {
+  return ;
+}
+   inorder(root->left);
+   cout<<root->data<<" ";
+   inorder(root->right);
+ }
+// post order
+void postorder(Node* root) {
+  if(root == NULL) {
+  return ;
+}
+   postorder(root->left);
+   postorder(root->right);
+   cout<<root->data<<" ";
+ }
+  
+//level order
+void levelOrder(Node* root) {
+  queue<Node*> q;
+  q.push(root);
+  while(q.size() >0) {
+    Node* curr = q.front();
+    q.pop();
+    cout<<curr->data<<" ";
+    if(curr->left != NULL) {
+      q.push(curr->left);
+    }
+    if(curr->right != NULL) {
+      q.push(curr->right);
+    }
+  }
+  cout<<endl;
 }
 int main() {
   vector<int> preorder = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
   Node* root = buildTree(preorder);
-  cout<<root->data<<endl;
-  cout<<root->left->data<<endl;
-  cout<<root->right->data<<endl;
-  cout<<root->left->left->data<<endl;
-  cout<<root->left->right->data<<endl;
-  cout<<root->right->right->data<<endl;
+  levelOrder(root);
+  cout<<endl;
 
   return 0;
 }
